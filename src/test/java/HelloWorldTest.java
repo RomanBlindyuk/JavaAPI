1721d72491f0dd6179c04e0598ecd8130ac81b7c
RomanBlnd <erket.r@gmail.com>
2025-03-07 09:53:40 +0200

    params added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,14 +3,23 @@ import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
+        System.out.println("Response code: " + response.getStatusCode());
     }
 
 }
