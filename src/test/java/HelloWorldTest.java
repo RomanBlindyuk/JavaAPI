653895d11a06e3874605c17f25bab64a803595e1
RomanBlnd <erket.r@gmail.com>
2025-03-09 13:03:31 +0200

    Get cookie another variant

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -21,16 +21,8 @@ public class HelloWorldTest {
                 .post("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
 
-        System.out.println("\nPretty text:");
-        response.prettyPrint();
-
-        System.out.println("\nHeaders:");
-        Headers responseHeaders = response.getHeaders();
-        System.out.println(responseHeaders);
-
-        System.out.println("\nCookies");
-        Map<String, String> responseCookies = response.getCookies();
-        System.out.println(responseCookies);
+        String responseCookie = response.getCookie("auth_cookie");
+        System.out.println(responseCookie);
 
     }
 
